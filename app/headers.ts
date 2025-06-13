bcc7c4076df5924d37e67bccebb33288b9d9043a
Ali Almasi <a710almasi@gmail.com>
2025-06-13 13:38:25 +0330

    added charset in headers

--- app/headers.ts
+++ app/headers.ts
@@ -1,5 +1,5 @@
 export default {
-  "Content-Type": "application/json",
+  "Content-Type": "application/json; charset=UTF-8",
   "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
   Pragma: "no-cache",
   Expires: "0",
