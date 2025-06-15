d46370507681338c2c0e0721e866adbae41d840c
Ali Almasi <a710almasi@gmail.com>
2025-06-16 00:07:04 +0330

    middleware optimized

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,53 @@
 import { NextRequest, NextResponse } from "next/server";
+import { failOptions } from "./app/headers";
+
+function allowedPath(pathname: string): boolean {
+  if (pathname)
+    switch (pathname) {
+      case "/":
+      case "/allAnswers":
+      case "/getAnswer":
+      case "/style.css":
+        return true;
+
+      default:
+        return false;
+    }
+  else
+    throw new Error(
+      "Error while reading 'pathname' for checking allowed paths."
+    );
+}
 
 export default function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const url = request.nextUrl.clone();
 
-  if (pathname === "/allAnswers" || pathname === "/getAnswer")
-    return NextResponse.next();
+  try {
+    if (allowedPath(pathname)) return NextResponse.next();
 
-  if (pathname === "/") return NextResponse.next();
+    if (pathname.includes("/allAnswer")) {
+      url.pathname = "/allAnswers";
+      return NextResponse.redirect(url);
+    }
 
-  if (pathname.includes("/allAnswer")) {
-    url.pathname = "/allAnswers";
+    if (pathname.includes("/getAnswer")) {
+      url.pathname = "/getAnswer";
+      return NextResponse.redirect(url);
+    }
+
+    url.pathname = "/";
     return NextResponse.redirect(url);
+  } catch (error: any) {
+    NextResponse.json(
+      JSON.stringify({
+        status: "fail",
+        message:
+          error.message ||
+          "An unknown error occurred while processing your request",
+      }),
+      failOptions()
+    );
+    console.error(error.message);
   }
-
-  if (pathname.includes("/getAnswer")) {
-    url.pathname = "/getAnswer";
-    return NextResponse.redirect(url);
-  }
-
-  url.pathname = "/";
-  return NextResponse.redirect(url);
 }
