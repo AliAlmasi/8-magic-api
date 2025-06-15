694c03e4fb1f1c75a7f701d4b6d3f679d5f7ea9a
Ali Almasi <a710almasi@gmail.com>
2025-06-15 23:54:30 +0330

    optimizing root page & adding style.css

--- app/route.ts
+++ app/route.ts
@@ -8,40 +8,22 @@ export async function GET() {
   const filePath = path.join(process.cwd(), `readme.md`);
 
   try {
-    const markdownContent = fs.readFileSync(filePath, "utf8");
-    const processedContent = await remark().use(html).process(markdownContent);
-    const contentHtml = `
+    const markdownContent: string = fs.readFileSync(filePath, "utf8");
+    const processedContent: any = await remark()
+      .use(html)
+      .process(markdownContent);
+    const contentHtml: string = `
     <!doctype html>
     <html lang="en">
       <head>
         <title>8-magic-api</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-          body {
-            font-family: system-ui;
-            background-color: #f0f0f0;
-            font-size: 16px;
-          }
-          a:link, a:visited {
-            color: darkblue;
-            text-decoration: none;
-          }
-          a:hover, a:active, a:focus {
-            text-decoration: underline 1px;
-          }
-          code {
-            border: 1px solid gray;
-            padding: 2px 4px;
-          }
-        </style>
+        <link rel="stylesheet" href="/style.css">
       </head>
-      <body>
-        ${processedContent}
-      </body>
+      <body>${processedContent}</body>
     </html>
-    `;
-
+    `.replace(/[^<]+/g, (str) => str.replace(/\s+/, " "));
     return new Response(
       contentHtml,
       successOptions("Welcome", 200, "text/html")
